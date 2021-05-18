4687ed3780ff7afee24a0021bf15f273f12c1262
Andrew Wiles <andrew35@pdx.edu>
2021-05-17 19:39:43 -0700

    added example OMDb API query to the results page for reference, notes about the API in the Resources file

--- frontend-project/src/Pages/Results.js
+++ frontend-project/src/Pages/Results.js
@@ -1,7 +1,9 @@
 import { params } from "../App";
 import "./Results.css";
+import { useState, useEffect } from "react";
 
 let search = "";
+let result;
 
 export default function Results() {
   // Check if we are searching for the first time or a new search
@@ -9,23 +11,45 @@ export default function Results() {
     search = params;
   }
 
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://www.omdbapi.com/?s=" + search + "&apikey=5371282f")
+      .then((res) => res.json())
+      .then((res) => {
+        // example of how to get the search results from response
+        console.log(res["Search"]);
+
+        // example of how to get a singular result from the response
+        result = res["Search"][0]["Title"];
+
+        // if the request has returned data we can use
+        if (result) setLoading(false);
+      })
+      .catch((error) => console.log("fetch error:", error));
+  }, []);
+
+  // Give user some visual feedback while we are waiting on our request to return
+  if (isLoading && search !== "") {
+    return (
+      <div>
+        {/* <h1>Results</h1> */}
+        <h1>Loading...</h1>
+      </div>
+    );
+  } else if (!isLoading && search === "") {
+    return (
+      <div>
+        {/* <h1>Results</h1> */}
+        <h1>Invalid query</h1>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Results</h1>
-      <Child name={params} />
-    </div>
-  );
-}
-
-/**
- * Just a function to test if we're getting the correct input from our search bar
- * @param {*} param0
- * @returns
- */
-function Child({ name }) {
-  return (
-    <div>
-      {name ? <h2>{name}</h2> : <h2>There is no name in the query string</h2>}
+      <h2>{result}</h2>
     </div>
   );
 }
