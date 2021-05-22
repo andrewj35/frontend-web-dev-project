b391ea4b66039e05a298abc22c107b5d9764859b
shashank114 <sshekhar@pdx.edu>
2021-05-22 14:25:40 -0700

    Updating Info for Searched movies

--- frontend-project/src/searchComponent/movieCard.js
+++ frontend-project/src/searchComponent/movieCard.js
@@ -5,14 +5,21 @@ export default function MovieCard(props) {
 
 
     let { each } = props;
+    //console.log(each);
+    let baseURL =
+    window.location.protocol + "//" + window.location.host + "/info/";
+
+
     return (
         <div className='card-container'>
-            <div className = "card">
+            <a href={baseURL + each.imdbID} className = "card">
                 <h1>{each.Title} ({each.Year})</h1>
+                
+
                 <p>IMDB rating: {each.imdbID}</p>
                 <img style = {{marginLeft:'40%'}}src={each.Poster} alt={`${each.Title}`} width="200" />
                 <hr />
-            </div>
+            </a>
 
         </div>
     )
