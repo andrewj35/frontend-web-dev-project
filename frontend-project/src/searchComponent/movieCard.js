7ff5406157a30feb2bea8919a64722343691d0b1
Andrew Wiles <andrew35@pdx.edu>
2021-05-24 16:01:38 -0700

    reverting files so I can pull

--- frontend-project/src/searchComponent/movieCard.js
+++ frontend-project/src/searchComponent/movieCard.js
@@ -1,21 +1,26 @@
-import './movieCard.css'
+import "./movieCard.css";
 
-export default function MovieCard(props) {
+export const MovieCard = (props) => {
+  let { each } = props;
+  //console.log(each);
+  let baseURL =
+    window.location.protocol + "//" + window.location.host + "/info/";
 
-
-
-    let { each } = props;
-    return (
-        <div className='card-container'>
-            <div className = "card">
-                <h1>{each.Title} ({each.Year})</h1>
-                <p>IMDB rating: {each.imdbID}</p>
-                <img style = {{marginLeft:'40%'}}src={each.Poster} alt={`${each.Title}`} width="200" />
-                <hr />
-            </div>
-
-        </div>
-    )
-}
-
-
+  return (
+    <div className="card-container">
+      <a href={baseURL + each.imdbID} className="card">
+        {/* <p>IMDB rating: {each.imdbID}</p> */}
+        <img
+          style={{ width: "100%", height: "100%" }}
+          src={each.Poster}
+          alt={`${each.Title}`}
+          width="200"
+        />
+        <h5 style={{ textAlign: "center", color: "black" }}>
+          {each.Title} ({each.Year})
+        </h5>
+      </a>
+    </div>
+  );
+};
+export default MovieCard;
