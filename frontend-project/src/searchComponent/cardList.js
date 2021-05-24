067cb6912b87da50b24020301591b1ecdece886e
vengala <vengala@pdx.edu>
2021-05-23 23:41:17 -0700

    added title description for  results page

--- frontend-project/src/searchComponent/cardList.js
+++ frontend-project/src/searchComponent/cardList.js
@@ -4,11 +4,14 @@ import {MovieCard} from './movieCard';
 
 export const CardList = (props) => {
     return (
+      <>
+      <h1>Search Results</h1>
         <div className = 'card-list'>
            {props.results.map(each => (
              <MovieCard key = {each.id} each = {each} />
            ))}              
         </div>
+        </>
 
     )
       
