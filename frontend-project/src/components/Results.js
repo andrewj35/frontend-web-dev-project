c64d9e5a1e578ce40795883a5c70ad1f98ae9bf1
Andrew Wiles <andrew35@pdx.edu>
2021-05-25 16:51:05 -0700

    committing so I can pull

--- frontend-project/src/components/Results.js
+++ frontend-project/src/components/Results.js
@@ -1,51 +1,80 @@
-// import { params } from "../App";
-import "./Results.css";
 import { useState, useEffect } from "react";
-import { CardList } from "../searchComponent/cardList";
+import PosterGrid from "./PosterGrid";
+import Pagination from "react-js-pagination";
 
 export default function Results(props) {
-  const [isLoading, setLoading] = useState(true);
-  const [resultsArray, setResults] = useState([]);
+  const [loading, setLoading] = useState(true);
+  let [resultsArray, setResults] = useState([]);
+  let [page, setPage] = useState(parseInt(props["match"]["params"]["page"]));
+  const [total_pages, setTotal_pages] = useState(1);
+
+  function changePage(event) {
+    window.history.pushState(
+      {},
+      "",
+      window.location.protocol +
+        "//" +
+        window.location.host +
+        "/results/" +
+        props["match"]["params"]["title"] +
+        "/" +
+        event
+    );
+    setPage(event);
+  }
 
   useEffect(() => {
     const fetchMedia = async () => {
+      setLoading(true);
       fetch(
-        "https://www.omdbapi.com/?s=" +
+        "https://api.themoviedb.org/3/search/multi?query=" +
           props["match"]["params"]["title"] +
-          "&apikey=5371282f"
+          "&api_key=b0011e93f013cfbed3110a3729a3e3c5&language=en-US&page=" +
+          page
       )
         .then((res) => res.json())
         .then((res) => {
-          // example of how to get the search results from response
-          setResults(res["Search"]);
-
-          // if the request has returned data we can use
-          if (resultsArray) setLoading(false);
+          setTotal_pages(parseInt(res["total_pages"]));
+          setResults(res["results"]);
+          setLoading(false);
         })
         .catch((error) => console.log("fetch error:", error));
     };
     fetchMedia();
-  }, [props, resultsArray]);
+  }, [props, page]);
 
-  // Give user some visual feedback while we are waiting on our request to return
-  if (isLoading) {
-    return (
-      <div>
-        <h1>Loading...</h1>
-      </div>
-    );
-  } else if (resultsArray === undefined) {
+  if (loading) {
+    return <></>;
+  }
+
+  if (resultsArray.length === 0) {
     return (
       <>
         <h2>No results</h2>
       </>
     );
   } else {
-    // resultsArray.map((each) => console.log(each));
     return (
-      <>
-        <CardList results={resultsArray} />
-      </>
+      <div>
+        <h1>Results for: {props["match"]["params"]["title"]}</h1>
+        <div className="container">
+          <PosterGrid media={resultsArray} loading={loading} param={"any"} />
+          <br />
+          <div className="pagination justify-content-center">
+            <Pagination
+              itemClass="page-item"
+              linkClass="page-link"
+              itemsCountPerPage={20}
+              activePage={page}
+              totalItemsCount={total_pages * 20}
+              pageRangeDisplayed={5}
+              onChange={(event) => changePage(event)}
+              hideNavigation={true}
+            />
+          </div>
+          <br />
+        </div>
+      </div>
     );
   }
 }
