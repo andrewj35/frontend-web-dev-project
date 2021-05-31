ff5ef8f73788e4662490ebbd331c384289529c47
vengala <vengala@pdx.edu>
2021-05-30 17:21:38 -0700

    Added comments for search page

--- frontend-project/src/components/Results.js
+++ frontend-project/src/components/Results.js
@@ -23,6 +23,8 @@ export default function Results(props) {
     setPage(event);
   }
 
+  // console.log(id);
+
   useEffect(() => {
     const fetchMedia = async () => {
       setLoading(true);
@@ -41,6 +43,8 @@ export default function Results(props) {
           setLoading(false);
         })
         .catch((error) => console.log("fetch error:", error));
+        
+        // console.log(res);
     };
     fetchMedia();
   }, [props, page]);
@@ -52,13 +56,19 @@ export default function Results(props) {
   if (resultsArray.length === 0) {
     return (
       <>
-        <h2>No results</h2>
+        <h2>
+          No results
+          {/* Displays no results when the results array is 0 */}
+          </h2>
       </>
     );
   } else {
     return (
       <div>
-        <h1>Results for: {props["match"]["params"]["title"]}</h1>
+        <h1>
+          Results for: {props["match"]["params"]["title"]}
+          {/* Dispalys results for the searched movie/Tv show */}
+          </h1>
         <div className="container">
           <PosterGrid media={resultsArray} loading={loading} param={"any"} />
           <br />
@@ -80,3 +90,62 @@ export default function Results(props) {
     );
   }
 }
+
+
+// Initially we used omdb api
+// import { params } from "../App";
+// import "./Results.css";
+// import { useState, useEffect } from "react";
+// import {CardList} from "../searchComponent/cardList";
+
+// export default function Results(props) {
+//   const [isLoading, setLoading] = useState(true);
+//   const [resultsArray, setResults] = useState([]);
+
+//   useEffect(() => {
+//     const fetchMedia = async () => {
+//       fetch(
+//         "https://www.omdbapi.com/?s=" +
+//           props["match"]["params"]["title"] +
+//           "&apikey=5371282f"
+//       )
+//         .then((res) => res.json())
+//         .then((res) => {
+//           // example of how to get the search results from response
+//           setResults(res["Search"]);
+
+//           // if the request has returned data we can use
+//           if (resultsArray) setLoading(false);
+//         })
+//         .catch((error) => console.log("fetch error:", error));
+//     };
+//     fetchMedia();
+//   }, [props, resultsArray]);
+
+//   // Give user some visual feedback while we are waiting on our request to return
+//   if (isLoading) {
+//     return (
+//      <div>
+//         <h1>
+//            Loading...
+//          </h1>
+//        </div>
+//     );
+//   } else if (resultsArray === undefined) {
+//    return (
+//      <>
+//        <h2>
+//          No results
+//         </h2>
+//       </>
+//     );
+//   } else {
+//     // resultsArray.map((each) => console.log(each));
+//     return (
+//       <>
+//        <CardList results = 
+//               {resultsArray} />
+//      </>
+//     );
+//   }
+// }
