4481d72c81f054c3f7572375d0ce19c0c864582b
vengala <vengala@pdx.edu>
2021-06-03 17:15:51 -0700

    updated about page

--- frontend-project/src/components/About.js
+++ frontend-project/src/components/About.js
@@ -1,60 +1,65 @@
 import "../styles/About.css";
 export default function About() {
-  // console.log(props["match"]["params"]["pageNumber"]);
-  return (
-    <div className="About">
-      <h1>About</h1>
-      <div>
-        <div className="info" width="800px">
+
+    return (
+      <div className= "About">
+        <h1>
+          About
+          </h1>
+          <div className="info">
           <p>
-            Hello! Welcome to our Movie Dashboard Application. This Movie
-            Dashbaord Application is developed by Andrew Wiles, Shashank Sekhar
-            and Jaya Bhargavi Vengala.
-            <br />
-            We are students at Portland State University. We created this
-            project for our Front-End Web Development course (CS 410P/510) under
-            Caterina Paun.
+            <center>
+            Hello! Welcome to our Movie Dashboard Application. <br/>
+            This Movie Dashbaord Application is developed by Andrew Wiles, Shashank Sekhar and Jaya Bhargavi Vengala. <br/>
+            We are students at Portland State University and we created this project for our Front-end Course CS 410p/510 under Caterina Paun.
+            <br/> 
+            </center>
           </p>
-        </div>
-        <div className="description">
-          <h3>Description</h3>
-          <p>
-            This Dashboard application gives information about all the Movies,
-            TV shows.
-            <br />
-            When you search a movie or TV show, it gives information about the
-            cast, description, rating, release date and more! You can also
-            search for actors!
-            <br />
+          </div>
+          <div className="description">
+            <h3>
+              <center>Description</center>
+            </h3>
+            <p>
+              <center>
+            This Dashboard application gives information about all the Movies, Tv shows, Web series. 
+            <br/>
+            When you search a movie it gives information about the cast, description, Imdb rating, release date. 
+            </center> 
+            <br/>
             <center>
-              <br />
-              <b>Motive: </b>Users can know all the information about the
-              Movies, TV shows
+            <b>
+              Motive: </b>
+            Users can know all the information about the Movies, Tv shows
             </center>
-            <br />
+            <br/>
+              
             <center>
-              <b>Main Stack Used: </b>
-              HTML, CSS, JavaScript
-            </center>
-            <br />
+              <b>
+                Main Stack Used: </b>
+                 HTML, CSS, JavaScript 
+                </center>
+            <br/>
             <center>
-              <b>Libraries Used: </b>
-              Chart.js, react-js-pagination
-            </center>
-            <br />
+              <b>
+                Libraries Used: </b>
+                 Charts.js, react-js-pagination
+                </center> 
+            <br/>
             <center>
-              <b>APIs Used: </b>
-              The Movie Database, The Open Movie Database
-            </center>
-            <br />
+              <b>
+                APIs Used: </b>
+                 The Movie Database, The Open Movie Database 
+                </center>
+            <br/>
             <center>
-              <b>Frameworks Used: </b>
-              React, Bootstrap
-            </center>
-            <br />
-          </p>
-        </div>
+              <b>Frameworks Used: </b> 
+               React, Bootstrap 
+              </center>
+            <br/>
+          
+            </p>
+          </div>
       </div>
-    </div>
-  );
+    )
 }
